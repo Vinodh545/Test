d616595d5ca231ea1910f5ad47993b4a42834565
Vinodh545 <vinodhskvs1@gmail.com>
2025-01-04 22:54:17 +0530

    Modified HelloWorld.java 2nd time

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World. Welcome to Git Learning");
+		System.out.println("Practicing Git in Eclipse IDE");
 	}
 
 }
